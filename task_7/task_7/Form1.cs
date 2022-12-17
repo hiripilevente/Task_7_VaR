926c92f93d58cd742c3815a1a194b6159cf8edf5
hiripilevente <hiripilevi@gmail.com>
2022-12-17 14:00:25 +0100

    Get portfolio value (7)

--- task_7/task_7/Form1.cs
+++ task_7/task_7/Form1.cs
@@ -34,5 +34,19 @@ namespace task_7
 
             dataGridView2.DataSource = Portfolio;
         }
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
     }
 }
