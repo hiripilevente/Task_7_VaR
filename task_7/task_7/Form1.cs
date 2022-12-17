080df738669a80e0fe117ffc550760623cbdf14e
hiripilevente <hiripilevi@gmail.com>
2022-12-17 13:54:31 +0100

    Database connection + Data loading (3,4,5)

--- task_7/task_7/Form1.cs
+++ task_7/task_7/Form1.cs
@@ -4,17 +4,35 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using task_7.Entities;
 
 namespace task_7
 {
     public partial class Form1 : Form
     {
+        List<Tick> ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        PortfolioEntities context = new PortfolioEntities();
         public Form1()
         {
             InitializeComponent();
+            ticks = context.Tick.ToList();
+            dataGridView1.DataSource = ticks;
+            CreatePortfolio();
+
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
